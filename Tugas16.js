470d6ebfbe8af10a57c8eaae72aa174c4b81f352
susila tropika <64695610+susilaattari@users.noreply.github.com>
2021-09-07 06:12:07 +0700

    Tugas 16 Javascript Niomic

---
+++ Tugas16.js
@@ -0,0 +1,7 @@
+//Mencari nilai min dan max dari deretan yang ada di variabel
+
+let min =Math.min(45,100,99,3,2,4,1,2,3,19,22,14,10);
+let max = Math.max( 45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+
+console.log(min)
+console.log(max)
